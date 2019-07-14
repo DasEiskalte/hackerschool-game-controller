1fb6859f872fa096fbdef637da82fff1fa39a42b
a <a@b.de>
2019-07-14 15:38:07 +0200

    Added a stop

--- game_controller/bin/start_server.py
+++ game_controller/bin/start_server.py
@@ -18,7 +18,8 @@ async def server(websocket, path):
     while True:
         klassifikation = await hole_klassifikation(websocket)
         klasse = klassifikation.beste_klasse.name
-        print(f"< {klasse}")
+        if tastatur.ist_ausgabe_aktiv:
+            print(f"< {klasse}")
         tastatur.druecke_taste(Key.up, dauer_in_sekunden=1.0, prozent_aktiv=0.8)
         if klasse == 'links':
             tastatur.lasse_tasten_los([Key.left, Key.right])
